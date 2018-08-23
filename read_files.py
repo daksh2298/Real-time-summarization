4f3cb053164f7377c78f0d4295919feef4858ed7
daksh2298 <daksh2298@icloud.com>
2018-08-23 20:55:24 +0530

    read offline data

--- read_files.py
+++ read_files.py
@@ -8,26 +8,6 @@ for x in data:
 
 data.remove('rename.py')
 
-# count = 0
-# folders = ['01', '02', '24', '25', '26', '27', '28', '29', '30', '31']
-# for folder in folders:
-#     print('*' * 20, folder, '*' * 20)
-#     for file in os.listdir('../data/' + folder):
-#         print('-' * 15, '-' * 15, '\n')
-#         print('../data/{}/{}'.format(folder, file))
-#         print('-' * 15, '-' * 15, '\n')
-#         count += 1
-#         # os.rename(input_file, output_file)
-# print(count)
-# f = open('profile.json')
-# data = f.read()
-# profiles = json.loads(data)
-# for profile in profiles:
-#     tid = profile['id']
-#     file = open('../output/'+tid + '.txt', 'w')
-#
-# file.close()
-# f.close()
 def read_files():
     folders = ['01', '02', '24', '25', '26', '27', '28', '29', '30', '31']
     count=0
